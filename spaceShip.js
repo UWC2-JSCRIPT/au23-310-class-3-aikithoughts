30e0c417c6fdaa370ead899fd0a88a3812dc5a82
Dave Shevitz <shevitz@stripe.com>
2023-10-25 21:56:54 -0400

    add solution to SpaceShip

--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,23 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
+class SpaceShip {
+    constructor(name, topspeed) {
+        this.name = name;
+        this.topspeed = topspeed;
+    }
+    accelerate() {
+        const {name, topspeed} = this;
+        console.log(`${name} moving to ${topspeed}`)
+    }
+}
+
 
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const xwing = new SpaceShip("X-wing", "hyperspace");
+xwing.accelerate();
+const enterprise = new SpaceShip("Enterprise", "warp factor 10");
+enterprise.accelerate();
